23c64afa42ef1a96793270008013b98ba9d86e33
Stemu1 <mlefusteve@gmail.com>
2026-08-17 08:31:25 +0300

    Added a reset button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let clicks = 0;
 const countEl = document.getElementById("count");
 const button = document.getElementById("clickMe");
+const resetButton = document.getElementById("resetCount");
 
 button.addEventListener("click", () => {
   clicks += 1;
+  countEl.textContent = clicks;
+});
+
+resetButton.addEventListener("click", () => {
+  clicks = 0;
   countEl.textContent = clicks;
 });
